6d4491c562f09337a5dc44256956943504f86298
Kyle Lester <klester3@my.apsu.edu>
2019-03-12 23:54:58 -0500

    Added Basic Simon About

--- app/src/main/java/com/kyle_jason/myapplication/BasicSimon.java
+++ app/src/main/java/com/kyle_jason/myapplication/BasicSimon.java
@@ -36,7 +36,7 @@ public class BasicSimon extends Simon implements View.OnClickListener {
     private int success;
     private Handler handler;
     private boolean paused;
-    private boolean lockPlayButton;
+    private boolean lockPlayButton = true;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -96,7 +96,7 @@ public class BasicSimon extends Simon implements View.OnClickListener {
     //displays dialog box that informs about the game
     private void pressedAbout() {
         LayoutInflater inflater = getLayoutInflater();
-        View alertLayout = inflater.inflate(R.layout.random_about_dialog, null);
+        View alertLayout = inflater.inflate(R.layout.basic_about_dialog, null);
         AlertDialog.Builder quitAlert = new AlertDialog.Builder(this);
         quitAlert.setView(alertLayout);
         quitAlert.setCancelable(true);
--- app/src/main/java/com/kyle_jason/myapplication/ExtremeSimon.java
+++ app/src/main/java/com/kyle_jason/myapplication/ExtremeSimon.java
@@ -108,7 +108,7 @@ public class ExtremeSimon extends Simon implements View.OnClickListener {
     //displays dialog box that informs about the game
     private void pressedAbout() {
         LayoutInflater inflater = getLayoutInflater();
-        View alertLayout = inflater.inflate(R.layout.random_about_dialog, null);
+        View alertLayout = inflater.inflate(R.layout.extreme_about_dialog, null);
         AlertDialog.Builder quitAlert = new AlertDialog.Builder(this);
         quitAlert.setView(alertLayout);
         quitAlert.setCancelable(true);
